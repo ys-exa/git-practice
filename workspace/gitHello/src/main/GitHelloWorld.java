01c329d1b7fb8f435419f766ed7c4b302be681d2
yoshie <yoshie@DESKTOP-2QIORM5>
2018-08-01 21:10:18 +0900

    新規プロジェクト作成

---
+++ workspace/gitHello/src/main/GitHelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ *
+ */
+package main;
+
+/**
+ * Git HelloWorld
+ *
+ * @author
+ */
+public class GitHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
